bde213afad374858c326936503781e00d18c6b18
Surya Swanoff <suryathegreat.07@gmail.com>
2019-08-23 00:25:26 +0530

    USER: Added Search bus screen

--- users/App.js
+++ users/App.js
@@ -4,20 +4,27 @@ import Router from './Router';
 import firebase from 'firebase';
 
 export default class App extends Component {
-
-    componentWillMount() {
-      var firebaseConfig = {
-        apiKey: "AIzaSyAe0t2aRPZcutEBO7hjXp4rO-T-078QM2o",
-        authDomain: "bus-bench.firebaseapp.com",
-        databaseURL: "https://bus-bench.firebaseio.com",
-        projectId: "bus-bench",
-        storageBucket: "bus-bench.appspot.com",
-        messagingSenderId: "1003500493080",
-        appId: "1:1003500493080:web:f6ac4616aebe0188"
-      };
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+      
     }
+  }
+
+  componentWillMount() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyAe0t2aRPZcutEBO7hjXp4rO-T-078QM2o",
+      authDomain: "bus-bench.firebaseapp.com",
+      databaseURL: "https://bus-bench.firebaseio.com",
+      projectId: "bus-bench",
+      storageBucket: "bus-bench.appspot.com",
+      messagingSenderId: "1003500493080",
+      appId: "1:1003500493080:web:f6ac4616aebe0188"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 
   render() {
     return (
--- users/src/Screens/Home.js
+++ users/src/Screens/Home.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react'
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
 import {Actions} from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input, Button } from 'react-native-elements';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            board: '',
+            drop: ''
+        }
+    }
     render() {
         return (
-            <View>
-                <Text>Hello</Text>
+            <View style={searchBus.layout}>
+                <Text style={searchBus.title}>Search Bus</Text>
+                <Input
+                    placeholder='                     Boarding point'
+                    leftIcon={{ type: 'font-awesome', name: 'sign-in' }}
+                    onChangeText={(board) => this.setState({board})}
+                    value={this.state.board}
+                />
+
+                <Input
+                    placeholder = '                    Dropping point'
+                    leftIcon={{ type: 'font-awesome', name: 'sign-out' }}
+                    onChangeText={(drop) => this.setState({drop})}
+                    value={this.state.drop}
+                />
+
+                {/* <Text>{this.state.board}</Text> */}
+                <Button
+                    icon={
+                        <Icon
+                        name="search"
+                        size={15}
+                        color="white"
+                        />
+                    }
+                    title="   Search"
+                />
             </View>
         )
     }
 }
 
+const searchBus = StyleSheet.create({
+    layout: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        justifyContent: 'space-evenly',
+        padding: 20
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 25
+    }
+})
