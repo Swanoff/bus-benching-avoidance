8ced33e8071d5b2b6cf1ef319a687aa10a2ea324
Surya Swanoff <suryathegreat.07@gmail.com>
2019-08-25 01:45:41 +0530

    USER: Result sort based on distance / free seats

--- users/src/Screens/Results.js
+++ users/src/Screens/Results.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {View, Text, StyleSheet, TouchableNativeFeedback} from 'react-native';
+import {ButtonGroup} from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 // import firebase, { database } from 'firebase';
 
@@ -7,34 +8,87 @@ export default class Results extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            selectedIndex: 0
         }
+        this.updateIndex = this.updateIndex.bind(this);
+    }
+
+    updateIndex(selectedIndex) {
+        this.setState({
+            selectedIndex
+        })
     }
 
     render() {
         const gotoBusA = () => Actions.Track({busName: '96A'});
         const gotoBusB = () => Actions.Track({busName: '96B'});
 
+        const buttons = ['Distance', 'Free Seats'];
+        const {selectedIndex} = this.state;
+
         return (
             <View style={results.layout}>
                 <Text style={results.title}>2 buses found</Text>
 
                 <View>
-                    <TouchableNativeFeedback onPress={gotoBusA}>
-                        <View style={results.bus}>
-                            <Text style={results.busName}>96A</Text>
-                            <Text style={results.route}>Gandhipuram - Ettimadai</Text>
-                        </View>
-                    </TouchableNativeFeedback>
+                    <Text style={results.sortText}>Sort by: </Text>
+                    <View>
+                        <ButtonGroup 
+                            onPress={this.updateIndex}
+                            selectedIndex={selectedIndex}
+                            buttons={buttons}
+                            containerStyle={{height: 100}}
+                        />
+                    </View>
                 </View>
 
-                <View>
-                    <TouchableNativeFeedback onPress={gotoBusB}>
-                        <View style={results.bus}>
-                            <Text style={results.busName}>96B</Text>
-                            <Text style={results.route}>Gandhipuram - Ettimadai</Text>
+                {
+                    (this.state.selectedIndex==0)
+                    ?
+                    (
+                    <View>
+                        <View>
+                            <TouchableNativeFeedback onPress={gotoBusA}>
+                                <View style={results.bus}>
+                                    <Text style={results.busName}>96A</Text>
+                                    <Text style={results.route}>Gandhipuram - Ettimadai</Text>
+                                </View>
+                            </TouchableNativeFeedback>
                         </View>
-                    </TouchableNativeFeedback>
-                </View>
+
+                        <View>
+                            <TouchableNativeFeedback onPress={gotoBusB}>
+                                <View style={results.bus}>
+                                    <Text style={results.busName}>96B</Text>
+                                    <Text style={results.route}>Gandhipuram - Ettimadai</Text>
+                                </View>
+                            </TouchableNativeFeedback>
+                        </View>
+                    </View>
+                    )
+                    :
+                    (
+                    <View> 
+                        <View>
+                            <TouchableNativeFeedback onPress={gotoBusB}>
+                                <View style={results.bus}>
+                                    <Text style={results.busName}>96B</Text>
+                                    <Text style={results.route}>Gandhipuram - Ettimadai</Text>
+                                </View>
+                            </TouchableNativeFeedback>
+                        </View>
+
+                        <View>
+                            <TouchableNativeFeedback onPress={gotoBusA}>
+                                <View style={results.bus}>
+                                    <Text style={results.busName}>96A</Text>
+                                    <Text style={results.route}>Gandhipuram - Ettimadai</Text>
+                                </View>
+                            </TouchableNativeFeedback>
+                        </View>
+                    </View>
+                    )
+                }
             </View>
         )
     }
@@ -62,5 +116,15 @@ const results = StyleSheet.create({
     route: {
         fontWeight: '200',
         fontSize: 15
+    },
+    sortBox: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    sortText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20
     }
 })
